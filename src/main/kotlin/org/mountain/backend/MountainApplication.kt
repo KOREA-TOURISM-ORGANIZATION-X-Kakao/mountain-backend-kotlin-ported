54292b6f2e75ddf970d4077578d034e22737530e
devdynam0507 <wsnam0507@naver.com>
2021-10-05 23:53:46 +0900

    feat: JpaAuditing, AppProperties 활성화

--- src/main/kotlin/org/mountain/backend/MountainApplication.kt
+++ src/main/kotlin/org/mountain/backend/MountainApplication.kt
@@ -1,9 +1,14 @@
 package org.mountain.backend
 
+import org.mountain.backend.configuration.AppProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
+@EnableConfigurationProperties(AppProperties::class)
 open class MountainApplication
 
 fun main(args: Array<String>) {
