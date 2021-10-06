b55786bbebe2c352488ad072f846fc3f76174c4c
devdynam0507 <wsnam0507@naver.com>
2021-10-06 18:46:56 +0900

    feat: 컨트롤러 url mapping url 접두사 /api 추가

--- src/main/kotlin/org/mountain/backend/authentication/controller/AuthenticationController.kt
+++ src/main/kotlin/org/mountain/backend/authentication/controller/AuthenticationController.kt
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@RequestMapping("/authentication")
+@RequestMapping("/api/authentication")
 class AuthenticationController(
     val authenticationService: AuthenticationService
 ) {
