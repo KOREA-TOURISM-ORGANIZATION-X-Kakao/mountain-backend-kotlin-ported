a7a8ef9e59380841c65cfbbdd3ac8888baad2161
devdynam0507 <wsnam0507@naver.com>
2021-10-07 20:34:49 +0900

    feat: jwt filter, exception handler 구현

---
+++ src/main/kotlin/org/mountain/backend/authentication/jwt/JwtHandler.kt
@@ -0,0 +1,67 @@
+package org.mountain.backend.authentication.jwt
+
+import org.mountain.backend.authentication.service.UserService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.access.AccessDeniedException
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.AuthenticationEntryPoint
+import org.springframework.security.web.access.AccessDeniedHandler
+import org.springframework.stereotype.Component
+import org.springframework.web.filter.OncePerRequestFilter
+import javax.servlet.FilterChain
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+@Component
+class JwtAuthenticationEntryPoint : AuthenticationEntryPoint {
+
+    override fun commence(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        authException: AuthenticationException?
+    ) {
+        response!!.sendError(HttpServletResponse.SC_UNAUTHORIZED)
+    }
+
+}
+
+@Component
+class JwtAccessDeniedHandler : AccessDeniedHandler {
+    override fun handle(
+        request: HttpServletRequest?,
+        response: HttpServletResponse?,
+        accessDeniedException: AccessDeniedException?
+    ) {
+        response!!.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+}
+
+class JwtAuthenticationFilter (
+    private val jwtProvider: JwtProvider,
+) : OncePerRequestFilter() {
+
+    override fun doFilterInternal(
+        request: HttpServletRequest,
+        response: HttpServletResponse,
+        filterChain: FilterChain
+    ) {
+        val token = getToken(request)
+
+        if(token != null && token.isNotEmpty() && jwtProvider.validation(token)) run {
+            val authentication: Authentication = jwtProvider.getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+
+        filterChain.doFilter(request, response)
+    }
+
+    private fun getToken(request: HttpServletRequest): String {
+        return request.getHeader(jwtProvider.headerTokenKey)
+    }
+
+}
+
+
+
--- src/main/kotlin/org/mountain/backend/authentication/jwt/JwtProvider.kt
+++ src/main/kotlin/org/mountain/backend/authentication/jwt/JwtProvider.kt
@@ -4,8 +4,15 @@ import io.jsonwebtoken.*
 import org.mountain.backend.configuration.AppProperties
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.User
+import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.stereotype.Service
 import java.lang.IllegalArgumentException
+import java.lang.RuntimeException
 import java.util.*
 import javax.annotation.PostConstruct
 import kotlin.properties.Delegates
@@ -19,6 +26,8 @@ class JwtProvider (
 
     lateinit var tokenSecret: String
     var tokenExpirationSeconds: Long by Delegates.notNull()
+    val claimsKey = "authority"
+    val headerTokenKey = "X-AUTH-TOKEN"
 
     @PostConstruct
     fun init() {
@@ -32,7 +41,7 @@ class JwtProvider (
 
         return Jwts.builder()
             .setSubject(subject)
-            .claim("auth", authorities)
+            .claim(claimsKey, authorities)
             .setExpiration(expiresIn)
             .signWith(SignatureAlgorithm.HS256, tokenSecret)
             .compact()
@@ -43,6 +52,15 @@ class JwtProvider (
             .setSigningKey(tokenSecret)
             .parseClaimsJws(token)
             .body
+    }
+
+    fun getAuthentication(token: String): Authentication {
+        val claims = parseClaims(token)
+        val authority: String = claims.get(claimsKey).toString() ?: throw RuntimeException("권한정보가 없는 토큰입니다.")
+        val authorities: Collection<out GrantedAuthority> = listOf(SimpleGrantedAuthority(authority))
+        val userDetails: UserDetails = User(claims.subject, "", authorities)
+
+        return UsernamePasswordAuthenticationToken(userDetails, "", authorities)
     }
 
     fun validation(token: String): Boolean {
