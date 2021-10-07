08c2b9f77a86f5c075b4e7d1b7c810e113d068c2
devdynam0507 <wsnam0507@naver.com>
2021-10-08 02:43:17 +0900

    feat: test page 삭제, 테스트 컨트롤러 삭제

--- src/main/kotlin/org/mountain/backend/MountainApplication.kt
+++ src/main/kotlin/org/mountain/backend/MountainApplication.kt
@@ -5,24 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
-import org.springframework.stereotype.Controller
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.servlet.config.annotation.EnableWebMvc
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableConfigurationProperties(AppProperties::class)
-@Controller
-class MountainApplication {
-
-    @GetMapping("/")
-    fun index(): String {
-        return "index.html"
-    }
-
-}
+class MountainApplication
 
 fun main(args: Array<String>) {
     runApplication<MountainApplication>(*args)
